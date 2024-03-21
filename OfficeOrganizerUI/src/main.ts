bc6fb5908fbef73393905b1b0d0e121ef989591d
Natlinux81 <info@wenske-services-development.de>
2024-03-21 01:55:25 +0100

    Containerize test ready

--- OfficeOrganizerUI/src/main.ts
+++ OfficeOrganizerUI/src/main.ts
@@ -16,6 +16,9 @@ const providers = [
 
 if (environment.production) {
   enableProdMode();
+  if (window) {
+    window.console.log = function() {};
+  }
 }
 
 platformBrowserDynamic(providers).bootstrapModule(AppModule)
